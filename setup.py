dc3bc0a6de1382cc7da3b2c58442fae1381b95c4
las-palmas1 <las-palmas1@mail.ru>
2017-04-19 00:01:34 +0300

    start

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='tecplot_lib',
+    version='0.1',
+    py_modules=['tecplot_lib'],
+    url='',
+    license='',
+    author='Alexander Zhigalkin',
+    author_email='',
+    description='Python wrap for some TecPlot scripting language functions'
+)
+
